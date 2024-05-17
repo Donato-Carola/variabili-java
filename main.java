b7c98be6af3a002e2a1fbf7152927bfc3b502ac6
Donato Carola <don94@live.it>
2024-05-17 17:50:07 +0200

    inizio corso java - tipi di variabili

---
+++ main.java
@@ -0,0 +1,30 @@
+
+// //ESERCIZI su dichiarazione variabili
+
+// Fare tutti gli esercizi e solo dopo andare avanti.
+
+// 
+
+
+
+public class main {
+
+    public static void main(String[] args) {
+        // - Creare una variabile del tipo opportuno per contenere una media aritmetica
+        double media;
+        // - Creare una variabile del tipo opportuno per contenere una potenza del due
+        int potenza;
+        // - Creare una variabile del tipo opportuno per contenere una frazione
+
+        float frazione;
+        // - Creare una variabile del tipo opportuno per contenere il peso di una
+        // persona
+        float peso;
+// - Creare una variabile intera e assegnare come valore 30
+
+int num = 30;
+
+
+
+    }
+}
